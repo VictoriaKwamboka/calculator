16cdaed374ffb060d933712f9cbdea42d268c29e
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-03 07:30:41 +0300

    Add files to use in the project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var add = function(no1, no2){
+    return( no1 +no2)
+    };
+    
+    //alert(add(23,35));
+    
+    const subtract = (no1, no2) => no1 - no2;
+    
+    //alert (subtract(67,23))
+    
+    
+    const addition= (number1, number2) => number1 +number2;
+    
+    var number1 = parseInt(prompt('Enter a number: '));
+    var number2 = parseInt(prompt('Enter another number: '));
+    
+    alert('The sum of the two numbers is ' + addition(number1, number2))
