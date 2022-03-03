faaf5ad44b89e75d4ef3b486e33f8b5314b645fc
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-03 07:43:43 +0300

    Add a function to subtract and one to multiply 2 numbers

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
-var add = function(no1, no2){
-    return( no1 +no2)
-    };
-    
-    //alert(add(23,35));
-    
-    const subtract = (no1, no2) => no1 - no2;
-    
-    //alert (subtract(67,23))
-    
-    
-    const addition= (number1, number2) => number1 +number2;
-    
-    var number1 = parseInt(prompt('Enter a number: '));
-    var number2 = parseInt(prompt('Enter another number: '));
-    
-    alert('The sum of the two numbers is ' + addition(number1, number2))
+var add = function (no1, no2) {
+    return (no1 + no2)
+};
+
+//alert(add(23,35));
+
+const subtract = (no1, no2) => no1 - no2;
+
+//alert (subtract(67,23))
+
+//addition
+
+const addition = (number1, number2) => number1 + number2;
+
+var number1 = parseInt(prompt('Enter a number: '));
+var number2 = parseInt(prompt('Enter another number: '));
+
+
+
+//subtraction
+
+var subtraction = (no1, no2) => no1 - no2;
+
+//multiplication
+const multiply = (number1, number2) => number1 * number2
+
+//division
+
+
+
+alert(additon(number1,number2))
