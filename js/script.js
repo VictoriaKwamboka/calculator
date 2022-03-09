1063beb9910242cf6803d507d51b12aad9929dde
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-09 14:44:14 +0300

    Separate back-end and front-end logic

--- js/script.js
+++ js/script.js
@@ -4,29 +4,17 @@ var add = function (no1, no2) {
 
 //alert(add(23,35));
 
+//BACK-END LOGIC
+
 const subtract = (no1, no2) => no1 - no2;
-
-//alert (subtract(67,23))
-
-//addition
-
 const addition = (number1, number2) => number1 + number2;
-
-var number1 = parseInt(prompt('Enter a number: '));
-var number2 = parseInt(prompt('Enter another number: '));
-
-
-
-//subtraction
-
 const subtraction = (number1, number2) => number1 - number2;
-
-//multiplication
 const multiply = (number1, number2) => number1 * number2
-
-//division
 const divide = (number1, number2) => number1/number2
 
 
 
-alert(divide(number1,number2))
+//USER INTERFACE LOGIC
+var number1 = parseInt(prompt('Enter a number: '));
+var number2 = parseInt(prompt('Enter another number: '));
+alert(add(number1,number2))
